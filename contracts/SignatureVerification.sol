721b1137a71a0975f04ee2443ffaef551de746a9
Jr Buchynskyi <jrbuchynskyi@MacBook-Pro-Jr.local>
2024-02-05 19:13:44 +0100

    Blast adoption

--- contracts/SignatureVerification.sol
+++ contracts/SignatureVerification.sol
@@ -10,6 +10,52 @@ error InvalidSignatureError();
 error SignatureAlreadyUsedError();
 error RefereeIsUserError();
 
+enum YieldMode {
+    AUTOMATIC,
+    VOID,
+    CLAIMABLE
+}
+
+enum GasMode {
+    VOID,
+    CLAIMABLE 
+}
+
+interface IBlast{
+    // configure
+    function configureContract(address contractAddress, YieldMode _yield, GasMode gasMode, address governor) external;
+    function configure(YieldMode _yield, GasMode gasMode, address governor) external;
+
+    // base configuration options
+    function configureClaimableYield() external;
+    function configureClaimableYieldOnBehalf(address contractAddress) external;
+    function configureAutomaticYield() external;
+    function configureAutomaticYieldOnBehalf(address contractAddress) external;
+    function configureVoidYield() external;
+    function configureVoidYieldOnBehalf(address contractAddress) external;
+    function configureClaimableGas() external;
+    function configureClaimableGasOnBehalf(address contractAddress) external;
+    function configureVoidGas() external;
+    function configureVoidGasOnBehalf(address contractAddress) external;
+    function configureGovernor(address _governor) external;
+    function configureGovernorOnBehalf(address _newGovernor, address contractAddress) external;
+
+    // claim yield
+    function claimYield(address contractAddress, address recipientOfYield, uint256 amount) external returns (uint256);
+    function claimAllYield(address contractAddress, address recipientOfYield) external returns (uint256);
+
+    // claim gas
+    function claimAllGas(address contractAddress, address recipientOfGas) external returns (uint256);
+    function claimGasAtMinClaimRate(address contractAddress, address recipientOfGas, uint256 minClaimRateBips) external returns (uint256);
+    function claimMaxGas(address contractAddress, address recipientOfGas) external returns (uint256);
+    function claimGas(address contractAddress, address recipientOfGas, uint256 gasToClaim, uint256 gasSecondsToConsume) external returns (uint256);
+
+    // read functions
+    function readClaimableYield(address contractAddress) external view returns (uint256);
+    function readYieldConfiguration(address contractAddress) external view returns (uint8);
+    function readGasParams(address contractAddress) external view returns (uint256 etherSeconds, uint256 etherBalance, uint256 lastUpdated, GasMode);
+}
+
 contract SignatureVerification is Ownable{
 
     address immutable private i_owner;
@@ -24,6 +70,8 @@ contract SignatureVerification is Ownable{
     constructor() {
         i_owner = msg.sender;
         taskCount = 0;
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableYield();
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableGas();
     }
 
     function addTask(uint256 _prize) public onlyOwner {
@@ -82,6 +130,23 @@ contract SignatureVerification is Ownable{
         } else {
             revert InvalidSignatureError();
         }
+    }
+
+    /* Blast functions */
+    function claimAllGas(address recipient) external onlyOwner{
+		IBlast(0x4300000000000000000000000000000000000002).claimAllGas(address(this), recipient);
+    }
+    function claimMaxGas(address recipient) external onlyOwner{
+		IBlast(0x4300000000000000000000000000000000000002).claimAllGas(address(this), recipient);
+    }
+
+    /* Blast view functions */
+    function readClaimableYield() public view returns (uint256) {
+        return IBlast(0x4300000000000000000000000000000000000002).readClaimableYield(address(this));
+    }
+
+    function readGasParams() public view returns (uint256 etherSeconds, uint256 etherBalance, uint256 lastUpdated, GasMode) {
+        return IBlast(0x4300000000000000000000000000000000000002).readGasParams(address(this));
     }
 
 
